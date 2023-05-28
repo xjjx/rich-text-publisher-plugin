d2e976824b75c596bf697d78fc77fa8b35bd92b7
Pawel Piatek <pawelx.piatek@intel.com>
2023-05-28 10:57:09 +0200

    use addOrReplaceAction
    Use addOrReplaceAction instead of just addAction to allow
    to create dynamic reports on build sites.
    
    Signed-off-by: Pawel Piatek <pawelx.piatek@intel.com>

--- src/main/java/org/korosoft/jenkins/plugin/rtp/RichTextPublisher.java
+++ src/main/java/org/korosoft/jenkins/plugin/rtp/RichTextPublisher.java
@@ -253,7 +253,7 @@ public class RichTextPublisher extends Recorder implements SimpleBuildStep  {
         }
         
         AbstractRichTextAction action = new BuildRichTextAction(build, getMarkupParser().parse(replaceVars(text, vars)));
-        build.addAction(action);
+        build.addOrReplaceAction(action);
         build.save();
         
         listener.getLogger().println("RTP: Done!");
